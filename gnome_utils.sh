17040a402e7b1b194c433ea2a9472e117c275204
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-09-13 00:20:21 +0200

    Better GNOME function naming

--- gnome_utils.sh
+++ gnome_utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gs.ls() {
+gsettings.ls() {
 	for schema in $(gsettings list-schemas | grep -E "^org.gnome" --color=none | sort -u); do gsettings list-recursively $schema; done
 	return $?
 }
