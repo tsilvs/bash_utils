b6baf7bfae3c658014a5ed68ec2ba31778c3e898
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-09-15 11:59:17 +0100

    Aliases trimmed

--- alias_functions.sh
+++ alias_functions.sh
@@ -1,5 +1,75 @@
 #!/bin/bash
 
+neofetch.() {
+	fastfetch "$@"
+	return $?
+}
+
+fetch.() {
+	fastfetch "$@"
+	return $?
+}
+
+# ### Bookmarks
+# #alias cdmyrepo="cd /mnt/data/myrepo/"
+
+ls.() {
+	ls --color --group-directories-first "$@"
+	return $?
+}
+
+lsw() {
+	local dir="${1:?"Directory is required."}"
+	local name="${2:?"File name wildcard template is required."}"
+	find -L "${dir}" -wholename "${name}" -ls
+	return $?
+}
+
+catw() {
+	local dir="${1:?"Directory is required."}"
+	local name="${2:?"File name wildcard template is required."}"
+	find -L "${dir}" -wholename "${name}" -exec cat {} \;
+	return $?
+}
+
+lsd.() {
+	lsd -1 -A -G -X --color always --group-dirs first -l -g "$@"
+	return $?
+}
+
+eza.() {
+	eza -1AlF --color=always --icons=always --group-directories-first --smart-group --git-repos "$@"
+	return $?
+}
+
+md.() {
+	mkdir "$@"
+	return $?
+}
+
+rename.() {
+	prename "$@"
+	return $?
+}
+
+# mvall() {
+# 	local match="${1}"
+# 	local ext="${2}"
+# 	for file in $match; do mv "$file" "${file%.}.${ext}"; done
+# }
+
+# pgrep.() {
+# 	local pipe # TODO: Accept pipe stdin
+# 	local file # TODO: Accept file path
+# 	local regex="${1:?"Regex required."}"
+# 	perl -ne "print if /${regex}/"
+# 	return $?
+# }
+
+# #### For flatpaks
+
+# # alias appfp='flatpak run tld.author.app'
+
 af.flat.template() {
 	local command="${1:?"Command is required."}"
 	local package="${2:?"Package is required."}"
@@ -32,46 +102,17 @@ chromium() {
 	return $?
 }
 
-# pgrep.() {
-# 	local pipe # TODO: Accept pipe stdin
-# 	local file # TODO: Accept file path
-# 	local regex="${1:?"Regex required."}"
-# 	perl -ne "print if /${regex}/"
-# 	return $?
-# }
-
-# ## Aliases
-# ### Commands
-# #alias rot='rpm-ostree'
-# #alias rotpl=$'rot status -b | grep -iE -A5000 "LayeredPackages[:]" | sed \'s/[ ]/\\n/g;s/\(Layered\|Local\)Packages[:]//g\' | sort -u'
-# #alias rotpl='rot status -b --json | jq -r ''.deployments[0].packages[]'''
-# alias clear='clear; draw_hint'
-# alias md='mkdir'
-# #alias ls='ls --color --group-directories-first -h -1 -X'
-# alias ls='ls --color --group-directories-first'
-# alias tree='tree --dirsfirst'
-# alias neofetch='fastfetch'
-# alias fetch='fastfetch'
-# alias lsd='lsd -1 -A -G -X --color always --group-dirs first -l -g'
-# alias eza='eza -1AlF --color=always --icons=always --group-directories-first --smart-group --git-repos'
-# alias rename='prename'
-# #alias diff='diff -y --color=''always'''
-# alias gsls='for schema in $(gsettings list-schemas | grep -E "^org.gnome" --color=none | sort -u); do gsettings list-recursively $schema; done'
-
 # #### For distrobox apps
 
 # ##### Arch
 
 # alias gitbatch_db_a='distrobox enter arch -- gitbatch'
-
-# #### For toolbox apps
-
-# ##### Arch
 # alias pacmana='toolbox run --container arch pacman'
 # alias microa='toolbox run --container arch micro'
 # alias pandoca='toolbox run --container arch pandoc'
 
 # ##### Fedora
+
 # alias dnftb='toolbox run --container fedora sudo dnf'
 # alias rpmtb='toolbox run --container fedora sudo rpm'
 # alias yumtb='toolbox run --container fedora sudo yum'
@@ -101,10 +142,3 @@ chromium() {
 # alias pdfseparatetb='toolbox run --container fedora pdfseparate'
 # alias pdftocairotb='toolbox run --container fedora pdftocairo'
 # alias pdftopstb='toolbox run --container fedora pdftops'
-
-# #### For flatpaks
-
-# # alias appfp='flatpak run tld.author.app'
-
-# ### Bookmarks
-# #alias cdmyrepo="cd /mnt/data/myrepo/"
--- bash_utils.sh
+++ bash_utils.sh
@@ -1,53 +1,5 @@
 #!/bin/bash
 
-#alias ls='ls --color --group-directories-first'
-#alias lsd='lsd -1 -A -G -X --color always --group-dirs first -l -g'
-#alias eza='eza -1AlF --color=always --icons=always --group-directories-first --smart-group --git-repos'
-#alias md='mkdir'
-#alias tree='tree --dirsfirst'
-#alias rename='prename'
-
-ls.() {
-	ls --color --group-directories-first "$@"
-	return $?
-}
-
-# lsw() {
-# 	find -L "${path}" -wholename "${name}" -ls
-# 	return $?
-# }
-
-# catw() {
-# 	find -L "${path}" -wholename "${name}" -exec cat {} \;
-# 	return $?
-# }
-
-lsd.() {
-	lsd -1 -A -G -X --color always --group-dirs first -l -g "$@"
-	return $?
-}
-
-eza.() {
-	eza -1AlF --color=always --icons=always --group-directories-first --smart-group --git-repos "$@"
-	return $?
-}
-
-md.() {
-	mkdir "$@"
-	return $?
-}
-
-rename.() {
-	prename "$@"
-	return $?
-}
-
-# mvall() {
-# 	local match="${1}"
-# 	local ext="${2}"
-# 	for file in $match; do mv "$file" "${file%.}.${ext}"; done
-# }
-
 ls.git() {
 	[[ " $* " =~ ' --help ' ]] && {
 		echo -e "Usage: ls.git
@@ -143,3 +95,5 @@ commands() {
 draw_hint () {
 	echo -e '\033[47;30m^C\033[0m Interrupt\t\033[47;30m^Z\033[0m Suspend\t\033[47;30m^D\033[0m Close\n\033[47;30m^L\033[0m Clear Screen\t\033[47;30m^S\033[0m Stop Out\t\033[47;30m^Q\033[0m Resume Out\n\033[47;30m^K\033[0m Cut to end\t\033[47;30m^U\033[0m Cut to start\t\033[47;30m^Y\033[0m Paste\n\033[47;30m^P\033[0m Prev cmd\t\033[47;30m^N\033[0m Next cmd\t\033[47;30m^R\033[0m Srch hist\n'
 }
+
+# alias clear='clear; draw_hint'
