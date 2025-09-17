c43e6acf155ce9ed801b85323eb4419047d21ae4
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-09-17 14:05:54 +0100

    bash boilerplate trim

--- bash_boilerplate.sh
+++ bash_boilerplate.sh
@@ -3,10 +3,11 @@
 #function_example() {
 #	# Check any arg for being help flag
 #	# if you need at least some options: [[ ( (( $# == 0 )) ) || ( " $* " =~ ' --help ' ) ]]
+# 	local usage="Usage: ${FUNCNAME[0]} ARGUMENT [OPTIONAL]
+#	--help	Displays this help message"
 #	[[ " $* " =~ ' --help ' ]] && {
-#		echo -e "Usage: function_example ARGUMENT [OPTIONAL]
-#	--help	Displays this help message";
-#		return 0;
+#		echo -e "${usage}"
+#		return 0
 #	}
 #	local file_path="${1:-"$(pwd)/example_file.txt"}"
 #	local lines=$(<"${file_path}")
