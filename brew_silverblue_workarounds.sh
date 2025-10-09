e5b399819632f03a72cd8990067e68a3515b9e38
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-10-09 16:14:58 +0100

    Fixing var linking issues

--- brew_silverblue_workarounds.sh
+++ brew_silverblue_workarounds.sh
@@ -2,11 +2,11 @@
 
 # export TESSDATA_PREFIX="/home/linuxbrew/.linuxbrew/share/tessdata"
 
-TESSDATA_PREFIX="/home/linuxbrew/.linuxbrew/share/tessdata"
+TESSDATA_PREFIX_BREW="/home/linuxbrew/.linuxbrew/share/tessdata"
 
-[[ -d $TESSDATA_PREFIX ]] && {
+[[ -d $TESSDATA_PREFIX_BREW ]] && {
 	tesseract() {
-		TESSDATA_PREFIX=$TESSDATA_PREFIX /home/linuxbrew/.linuxbrew/bin/tesseract "$@"
+		TESSDATA_PREFIX="$TESSDATA_PREFIX_BREW" /home/linuxbrew/.linuxbrew/bin/tesseract "$@"
 		return $?
 	}
 }
