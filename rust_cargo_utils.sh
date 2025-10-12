b3f15697f904c006ed0ef40345f5a8d26a63b901
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-10-12 10:51:02 +0100

    Cargo exec permissions, shellscript vscode snippets

---
+++ rust_cargo_utils.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cargo.allow.exec() {
+	sudo chmod 755 /root
+	sudo chmod 755 /root/.cargo
+	sudo chmod 755 /root/.cargo/bin
+	# + add `/root/.cargo/bin` to PATH in: /etc/sudoers /etc/bashrc
+	# or use `sudo visudo` to edit `secure_path`
+}
+
