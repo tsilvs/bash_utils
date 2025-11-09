c8439bbc0b8a9dccd3bfe1b859566a18e083042a
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-11-09 22:30:38 +0000

    desktop mime type file association utils

--- desktop_utils.sh
+++ desktop_utils.sh
@@ -2,4 +2,15 @@
 
 desktop.ls.comm() {
 	comm -12 <(ls -1 /usr/share/applications/) <(ls -1 ~/.local/share/applications)
+	return $?
+}
+
+desktop.assoc.refresh() {
+	update-desktop-database ~/.local/share/applications/
+	return $?
+}
+
+desktop.mime.refresh() {
+	update-mime-database ~/.local/share/mime/
+	return $?
 }
