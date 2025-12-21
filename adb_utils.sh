0a1fe232ac6627a25efca440848d09f417842ca6
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-12-21 19:58:29 +0100

    ADB Utils - more consistent function names - better params - bulk (un)installing apps for all users

--- adb_utils.sh
+++ adb_utils.sh
@@ -1,102 +1,158 @@
 #!/usr/bin/env bash
 
+# SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
 adb.users() {
+	[[ "$1" =~ ^(-h|--help)$ ]] && { echo "Usage: ${FUNCNAME[0]} <device_id>"; return 0; }
 	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	adb -s $device shell pm list users | tail -n +2 | awk -F "[{:}]" '{print $2}'
+	adb -s "$device" shell pm list users | tail -n +2 | awk -F "[{:}]" '{print $2}'
 }
 
-adb.u() {
-	adb.users $@
+adb.app.ls() {
+	local device="" user=0
+	
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h|--help)
+				cat <<-EOF
+				Usage: ${FUNCNAME[0]} <device_id> [options]
+				Options:
+				  -u|--user <id>  User ID (default: 0)
+				EOF
+				return 0
+				;;
+			-u|--user) user="$2"; shift 2 ;;
+			*) [[ -z "$device" ]] && { device="$1"; shift; } || shift ;;
+		esac
+	done
+	
+	[[ -z "$device" ]] && { echo "Device ID required"; return 1; }
+	[[ ! "$user" =~ ^[0-9]+$ ]] && { echo "User ID must be a number"; return 1; }
+	
+	adb -s "${device}" shell cmd package list packages --user "${user}" | sed 's/^package://g' | sort
 }
 
-# adb.pair() {
-# 	 "$@"
-# 	return $?
-# }
+adb.apps.diff() {
+	local device="" u1=0 u2=10 filter=""
+	
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h|--help)
+				cat <<-EOF
+				Usage: ${FUNCNAME[0]} <device_id> [options]
+				Options:
+				  -u1 <id>      First user ID (default: 0)
+				  -u2 <id>      Second user ID (default: 10)
+				  -m|--matching Only show matching packages
+				  -a|--added    Only show packages added in u2
+				  -r|--removed  Only show packages removed from u2
+				EOF
+				return 0
+				;;
+			-u1) u1="$2"; shift 2 ;;
+			-u2) u2="$2"; shift 2 ;;
+			-m|--matching) filter="matching"; shift ;;
+			-a|--added) filter="added"; shift ;;
+			-r|--removed) filter="removed"; shift ;;
+			*) [[ -z "$device" ]] && { device="$1"; shift; } || shift ;;
+		esac
+	done
+	
+	[[ -z "$device" ]] && { echo "Device ID required"; return 1; }
+	[[ ! "$u1" =~ ^[0-9]+$ ]] && { echo "User 1 ID must be a number"; return 1; }
+	[[ ! "$u2" =~ ^[0-9]+$ ]] && { echo "User 2 ID must be a number"; return 1; }
+	
+	local diff_output
+	diff_output=$(diff -u \
+		<(adb.app.ls "${device}" -u "${u1}") \
+		<(adb.app.ls "${device}" -u "${u2}"))
 
-adb.diff.apps() {
-	# TODO:
-	# + add options to filter out:
-	# 	+ only matching
-	# 	+ only added
-	# 	+ only removed
-	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	local u1="${2:-0}"; [[ ! "$u1" =~ ^[0-9]+$ ]] && { echo "User 1 ID must be a number"; return 1; }
-	local u2="${3:-10}"; [[ ! "$u2" =~ ^[0-9]+$ ]] && { echo "User 2 ID must be a number"; return 1; }
-	diff -u \
-		<(adb -s "${device}" shell cmd package list packages --user "${u1}") \
-		<(adb -s "${device}" shell cmd package list packages --user "${u2}") \
-		| perl -ne 'print if /^[+-]/' \
-		| sed 's/package://g'
+	case "$filter" in
+		matching) echo "$diff_output" | grep -v '^[+-]' ;;
+		added) echo "$diff_output" | grep '^+' | grep -v '^+++' | sed 's/^+//' ;;
+		removed) echo "$diff_output" | grep '^-' | grep -v '^---' | sed 's/^-//' ;;
+		*) echo "$diff_output" | perl -ne 'print if /^[+-]/' | sed 's/^[+-]//' ;;
+	esac
 }
 
-adb.ifu() {
-	# TODO:
-	# + Accept multiple
-	# 	+ Users - implemented in ifa
-	# 	+ Apps - done
-	# + Accept flags
-	# 	+ for all users
-	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	local u="${2:-0}"; [[ ! "$u" =~ ^[0-9]+$ ]] && { echo "User ID must be a number"; return 1; }
-	local argnum=2
-	shift ${argnum}
-	# for user in users
-	local apps=("$@"); [[ ${#apps[@]} -eq 0 ]] && { echo "At least one app package name is required"; return 1; }
-	for app in "${apps[@]}"; do
-		echo "Installing ${app} for user ${u} on device ${device}"
-		adb -s "${device}" shell cmd package install-existing --user "${u}" "${app}"
+adb.app.install() {
+	local device="" users=() apps=() all_users=false
+	
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h|--help)
+				cat <<-EOF
+				Usage: ${FUNCNAME[0]} <device_id> [options] <package...>
+				Options:
+				  -u|--user <id>  User ID (repeatable)
+				  -a|--all-users  Install for all users
+				EOF
+				return 0
+				;;
+			-u|--user) users+=("$2"); shift 2 ;;
+			-a|--all-users) all_users=true; shift ;;
+			*) [[ -z "$device" ]] && { device="$1"; shift; } || { apps+=("$1"); shift; } ;;
+		esac
+	done
+	
+	[[ -z "$device" ]] && { echo "Device ID required"; return 1; }
+	[[ ${#apps[@]} -eq 0 ]] && { echo "At least one package required"; return 1; }
+	
+	$all_users && users=($(adb.users "${device}"))
+	[[ ${#users[@]} -eq 0 ]] && { echo "User ID required"; return 1; }
+	
+	for u in "${users[@]}"; do
+		[[ ! "$u" =~ ^[0-9]+$ ]] && { echo "User ID must be a number: $u"; return 1; }
+		for app in "${apps[@]}"; do
+			echo "Installing ${app} for user ${u} on device ${device}"
+			adb -s "${device}" shell cmd package install-existing --user "${u}" "${app}"
+		done
 	done
 }
 
-adb.ifa() {
-	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	local argnum=1
-	shift ${argnum}
-	local apps=("$@"); [[ ${#apps[@]} -eq 0 ]] && { echo "At least one app package name is required"; return 1; }
-	for u in $(adb.u "${device}"); do
-		adb.ifu "${device}" "${u}" "${apps}"
+adb.app.uninstall() {
+	local device="" users=() apps=() all_users=false
+	
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h|--help)
+				cat <<-EOF
+				Usage: ${FUNCNAME[0]} <device_id> [options] <package...>
+				Options:
+				  -u|--user <id>  User ID (repeatable)
+				  -a|--all-users  Uninstall for all users
+				EOF
+				return 0
+				;;
+			-u|--user) users+=("$2"); shift 2 ;;
+			-a|--all-users) all_users=true; shift ;;
+			*) [[ -z "$device" ]] && { device="$1"; shift; } || { apps+=("$1"); shift; } ;;
+		esac
+	done
+	
+	[[ -z "$device" ]] && { echo "Device ID required"; return 1; }
+	[[ ${#apps[@]} -eq 0 ]] && { echo "At least one package required"; return 1; }
+	
+	$all_users && users=($(adb.users "${device}"))
+	[[ ${#users[@]} -eq 0 ]] && { echo "User ID required"; return 1; }
+	
+	for u in "${users[@]}"; do
+		[[ ! "$u" =~ ^[0-9]+$ ]] && { echo "User ID must be a number: $u"; return 1; }
+		for app in "${apps[@]}"; do
+			echo "Uninstalling ${app} for user ${u} on device ${device}"
+			adb -s "${device}" shell cmd package uninstall --user "${u}" "${app}"
+		done
 	done
 }
 
-adb.ufu() {
-	# TODO:
-	# + Accept multiple
-	# 	+ Users - implemented in ufa
-	# 	+ Apps - done
-	# + Accept flags
-	# 	+ for all users
-	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	local u="${2:-0}"; [[ ! "$u" =~ ^[0-9]+$ ]] && { echo "User ID must be a number"; return 1; }
-	local argnum=2
-	shift ${argnum}
-	# for user in users
-	local apps=("$@"); [[ ${#apps[@]} -eq 0 ]] && { echo "At least one app package name is required"; return 1; }
-	for app in "${apps[@]}"; do
-		echo "Uninstalling ${app} for user ${u} on device ${device}"
-		adb -s "${device}" shell cmd package uninstall --user "${u}" "${app}"
-	done
+adb.app.in() {
+	adb.app.install "$@"
+	return $?
 }
 
-adb.ufa() {
-	local device="${1:?"Device ID (Serial or IP address) is required"}"
-	local argnum=1
-	shift ${argnum}
-	local apps=("$@"); [[ ${#apps[@]} -eq 0 ]] && { echo "At least one app package name is required"; return 1; }
-	for u in $(adb.u "${device}"); do
-		adb.ufu "${device}" "${u}" "${apps}"
-	done
+adb.app.un() {
+	adb.app.uninstall "$@"
+	return $?
 }
 
-# jq '.[]["removal"]' --raw-output 0x192/universal-android-debloater/resources/assets/uad_lists.json | sort -u
-# adb.ufu $device $uid $(jq '.[] | select(.removal=="Recommended") | select(.id | match(".*google.*")) | .id' --raw-output 0x192/universal-android-debloater/resources/assets/uad_lists.json)
-
-export -f adb.users
-export -f adb.u
-# export -f adb.pair
-export -f adb.diff.apps
-export -f adb.ifu
-export -f adb.ifa
-export -f adb.ufu
-export -f adb.ufa
-
+export -f adb.users adb.apps.diff adb.app.install adb.app.uninstall adb.app.in adb.app.un adb.app.ls
