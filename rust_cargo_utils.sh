c00eb20a630fe63fa1b5bb114c4b9acdb0568734
Vsevolod Tsiliurik <tsilvs@proton.me>
2026-01-13 19:51:48 +0000

    small fix of naming

--- rust_cargo_utils.sh
+++ rust_cargo_utils.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-cargo.allow.exec() {
-	sudo chmod 755 /root
-	sudo chmod 755 /root/.cargo
-	sudo chmod 755 /root/.cargo/bin
+cargo.exec.allow() {
+	chmod 755 /root
+	chmod 755 /root/.cargo
+	chmod 755 /root/.cargo/bin
 	# + add `/root/.cargo/bin` to PATH in: /etc/sudoers /etc/bashrc
 	# or use `sudo visudo` to edit `secure_path`
 }
 
-export -f cargo.allow.exec
+export -f cargo.exec.allow
+
